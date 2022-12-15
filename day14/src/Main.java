cc53c458524c58aa251b4f3fa7fdd72f5127bbc4
MDupas <marin.dupas@sfr.fr>
2022-12-15 04:37:01 +0100

    day 14 done

--- day14/src/Main.java
+++ day14/src/Main.java
@@ -2,17 +2,17 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
 
-    private static final String input_url = "../inputs/day14/test.txt";
+    private static final String input_url = "../inputs/day14/input.txt";
 
     public static void main(String[] args) {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(input_url));
-            firstPuzzle(reader);
-//            secondPuzzle(reader);
+//            firstPuzzle(reader);
+            secondPuzzle(reader);
             reader.close();
         } catch (FileNotFoundException e) {
             System.err.println("File not found at " + input_url);
@@ -23,24 +23,137 @@ public class Main {
     }
 
     private static void firstPuzzle(BufferedReader reader) throws IOException {
-        ArrayList<Integer> lot_of_arguments = new ArrayList<>();
-        String[] arguments = reader.readLine().split(",");
-        for (String s : arguments) {
-            lot_of_arguments.add(Integer.parseInt(s));
+        Set<Point> obstacleSet = generateRocks(reader);
+        displayRocks(obstacleSet);
+        int sandCounter = 0;
+        Stack<Point> lastPos = new Stack<>();
+        int maxY = obstacleSet.stream().max(Comparator.comparingInt(Point::getY)).orElseThrow().getY();
+        boolean abyssFall = false;
+        while (!abyssFall) {
+            Point sand;
+            if (lastPos.empty())
+                sand = new Point(500, 0);
+            else {
+                sand = lastPos.pop();
+            }
+            while (true) {
+                if (sand.getY() > maxY)  {
+                    abyssFall = true;
+                    break;
+                } else if (!obstacleSet.contains(new Point(sand.getX(), sand.getY() + 1))) {
+                    lastPos.add(sand);
+                    sand = new Point(sand.getX(), sand.getY() + 1);
+                } else if (!obstacleSet.contains(new Point(sand.getX() - 1, sand.getY() + 1))) {
+                    lastPos.add(sand);
+                    sand = new Point(sand.getX() - 1, sand.getY() + 1);
+                } else if (!obstacleSet.contains(new Point(sand.getX() + 1, sand.getY() + 1))) {
+                    lastPos.add(sand);
+                    sand = new Point(sand.getX() + 1, sand.getY() + 1);
+                } else {
+                    if (!obstacleSet.contains(sand)) {
+                        sandCounter++;
+                        obstacleSet.add(sand);
+                    }
+                    break;
+                }
+            }
         }
+
+        System.out.println("Total number of sand falling : " + sandCounter);
+    }
+
+    private static Set<Point> generateRocks(BufferedReader reader) throws IOException {
+        Set<Point> rockSet = new HashSet<>();
         while (reader.ready()) {
-            int input = Integer.parseInt(reader.readLine());
+            String[] points = reader.readLine().split("->");
+            Point lastRock = null;
+            for (String point : points) {
+                String[] coordinates = point.trim().split(",");
+                Point newRock = new Point(Integer.parseInt(coordinates[0]),
+                        Integer.parseInt(coordinates[1]));
+                rockSet.add(newRock);
+                if (lastRock != null) {
+                    if (lastRock.getX() != newRock.getX()) {
+                        for (int x = Math.min(lastRock.getX(), newRock.getX());
+                             x <= Math.max(lastRock.getX(), newRock.getX()); x++) {
+                            Point linePoint = new Point(x, newRock.getY());
+                            rockSet.add(linePoint);
+                        }
+                    } else {
+                        for (int y = Math.min(lastRock.getY(), newRock.getY());
+                             y <= Math.max(lastRock.getY(), newRock.getY()); y++) {
+                            Point linePoint = new Point(newRock.getX(), y);
+                            rockSet.add(linePoint);
+                        }
+                    }
+                }
+                lastRock = newRock;
+            }
+        }
+        return rockSet;
+    }
+
+    private static void displayRocks(Set<Point> rockSet) {
+        int minX = rockSet.stream().min(Comparator.comparingInt(Point::getX)).orElseThrow().getX();
+        int minY = rockSet.stream().min(Comparator.comparingInt(Point::getY)).orElseThrow().getY();
+        int maxX = rockSet.stream().max(Comparator.comparingInt(Point::getX)).orElseThrow().getX();
+        int maxY = rockSet.stream().max(Comparator.comparingInt(Point::getY)).orElseThrow().getY();
+        System.out.print("__");
+        for (int i = minX; i <= maxX; i++) {
+            System.out.print((i % 10));
+        }
+        System.out.println();
+        for (int y = minY; y <= maxY; y++) {
+            System.out.print(y + ' ');
+            for (int x = minX; x <= maxX; x++) {
+                Point currentPoint = new Point(x,y);
+                if (rockSet.contains(currentPoint))
+                    System.out.print('#');
+                else
+                    System.out.print('.');
+            }
+            System.out.println();
         }
     }
 
     private static void secondPuzzle(BufferedReader reader) throws IOException {
-        ArrayList<Integer> lot_of_arguments = new ArrayList<>();
-        String[] arguments = reader.readLine().split(",");
-        for (String s : arguments) {
-            lot_of_arguments.add(Integer.parseInt(s));
+        Set<Point> obstacleSet = generateRocks(reader);
+        displayRocks(obstacleSet);
+        int sandCounter = 0;
+        Stack<Point> lastPos = new Stack<>();
+        int maxY = obstacleSet.stream().max(Comparator.comparingInt(Point::getY)).orElseThrow().getY();
+        Point source = new Point(500, 0);
+        while (!obstacleSet.contains(source)) {
+            Point sand;
+            if (lastPos.empty())
+                sand = new Point(500, 0);
+            else {
+                sand = lastPos.pop();
+            }
+            while (true) {
+                if (sand.getY() == maxY + 1)  {
+                    sandCounter++;
+                    obstacleSet.add(sand);
+                    break;
+                } else if (!obstacleSet.contains(new Point(sand.getX(), sand.getY() + 1))) {
+                    lastPos.add(sand);
+                    sand = new Point(sand.getX(), sand.getY() + 1);
+                } else if (!obstacleSet.contains(new Point(sand.getX() - 1, sand.getY() + 1))) {
+                    lastPos.add(sand);
+                    sand = new Point(sand.getX() - 1, sand.getY() + 1);
+                } else if (!obstacleSet.contains(new Point(sand.getX() + 1, sand.getY() + 1))) {
+                    lastPos.add(sand);
+                    sand = new Point(sand.getX() + 1, sand.getY() + 1);
+                } else {
+                    if (!obstacleSet.contains(sand)) {
+                        sandCounter++;
+                        obstacleSet.add(sand);
+                    }
+                    break;
+                }
+            }
         }
-        while (reader.ready()) {
-            int input = Integer.parseInt(reader.readLine());
-        }
+
+        System.out.println("Total number of sand falling : " + sandCounter);
     }
 }
---
+++ day14/src/Point.java
@@ -0,0 +1,42 @@
+public class Point implements Comparable<Point> {
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Point point = (Point) o;
+
+        if (x != point.x) return false;
+        return y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        if (this.getX() != o.getX())
+            return this.getX() - o.getY();
+        return this.getY() - o.getY();
+    }
+}
